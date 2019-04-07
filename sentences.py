ebd2e71d44654811bd08252b11b846b473a18439
monishij <47654586+monishij@users.noreply.github.com>
2019-04-07 04:05:44 -0700

    Add files via upload

---
+++ sentences.py
@@ -0,0 +1,27 @@
+# intros, health down, health up
+d = { "introduction" : ["You are now in the ___.",
+                        "You have been placed in the___",
+                        "You have moved to the ___",
+                        "You are now inside the ___",
+                        "You made your way over to the ___",
+                        "You've arrived in the ___",
+                        "Welcome to the ___. Prepare yourself!",
+                        "You've somehow teleported to the ___. I wonder how that works."
+                        "ETA to the ___ - zero seconds.",
+                        ],
+      "health down" : ["Your health has gone down.",
+                       "You have taken damage.",
+                       "You are not suited for this environment.",
+                       "Charles Darwin would be disappointed. You feel him curse you from th heavens.",
+                       "You are not superior here. You barely make it out alive.",
+                       ],
+      "health up" : ["Mother Nature would be proud. You feel Her presence give you power.",
+                     "Your features are exceptional! Health up!",
+                     "Your features are suitable for this environment. You feel more alive.",
+                     "Survival comes easy to you here.You feel invigorated.",
+                     "This environment is hardly a challenge with your features. You feel more energized than before.",
+                     "Charles Darwin seems to praise you from the heavens. You feel blessed.",
+                     "You're a force to be reckoned with here! Confidence flows through your body.
+                        ]
+      
+      }
